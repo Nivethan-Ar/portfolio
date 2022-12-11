199daa25eb2b73c9cd9718ba9a848958d90eae67
Nivethan <63112138+Nivethan-Ar@users.noreply.github.com>
2022-12-11 23:44:18 +0530

    fix: hover effect not working when hover over icons

--- src/pages/landing/components/right-aside/social-links.tsx
+++ src/pages/landing/components/right-aside/social-links.tsx
@@ -6,28 +6,28 @@ function SocialLinks() {
       [&>a>li>span]:hidden lg:[&>a>li>span]:block -mt-16
       '
       >
-        <a href='http://www.linkedin.com/in/nivethan-ar/' target='_blank' rel='noopener noreferrer'>
+        <a href='http://www.linkedin.com/in/nivethan-ar/' className="group" target='_blank' rel='noopener noreferrer'>
           <li className='flex items-center'>
             <img src='/images/linkedin.png' className='w-6 lg:w-8' />
-            <span className='text-white opacity-60  hover:opacity-100 font-bold'>
+            <span className='text-white opacity-60  group-hover:opacity-100 font-bold'>
               linkedin
             </span>
           </li>
         </a>
 
-        <a href='http://github.com/Nivethan-Ar' target='_blank' rel='noopener noreferrer'>
+        <a href='http://github.com/Nivethan-Ar' className="group" target='_blank' rel='noopener noreferrer'>
           <li className='flex items-center'>
             <img src='/images/github.png' className='w-6 lg:w-8' />
-            <span className='text-white opacity-60 hover:opacity-100 font-bold'>
+            <span className='text-white opacity-60 group-hover:opacity-100 font-bold'>
               github
             </span>
           </li>
         </a>
 
-        <a href='mailto:nivethan.me@gmail.com' target='_blank'>
+        <a href='mailto:nivethan.me@gmail.com' className="group" target='_blank'>
           <li className='flex items-center'>
             <img src='/images/email.png' className='w-6 lg:w-8' />
-            <span className='text-white opacity-60 hover:opacity-100 font-bold'>
+            <span className='text-white opacity-60 group-hover:opacity-100 font-bold'>
               email
             </span>
           </li>
