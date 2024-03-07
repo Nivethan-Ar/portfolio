3eeb9413f341291a6c98b13bce386aea270d3a7b
Nivethan-Ar <63112138+Nivethan-Ar@users.noreply.github.com>
2024-03-08 00:52:53 +0530

    feat: add basic PWA setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,53 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      manifest: {
+        name: 'PWA App',
+        short_name: 'PWA App',
+        description: 'My portfolio as PWA',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/images/pwa/manifest-icon-192.maskable.png',
+            sizes: '144x144',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '/images/pwa/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/images/pwa/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '/images/pwa/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/images/pwa/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
+});
